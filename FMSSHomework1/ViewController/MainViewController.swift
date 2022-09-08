141e2f7483013e9445126d08f2d28a7d8b80b00e
Tolga KÜÇÜK <tolga.kucuk13@gmail.com>
2022-09-08 22:50:42 +0300

    added table view to MainViewController

--- FMSSHomework1/ViewController/MainViewController.swift
+++ FMSSHomework1/ViewController/MainViewController.swift
@@ -27,7 +27,6 @@ class MainViewController: UIViewController {
 
 extension MainViewController: UITableViewDelegate {
     
-    
 }
 
 //MARK: - TableViewDataSource
@@ -42,6 +41,5 @@ extension MainViewController: UITableViewDataSource {
         cell.textLabel?.text = String(indexPath.row)
         return cell
     }
-
 }
 
